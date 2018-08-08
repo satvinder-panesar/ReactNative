0cbb173654d5f3854843fedd6498f005bb74690e
Satvinder Singh Panesar <panesar.satvindersingh@gmail.com>
2018-08-07 23:38:50 -0400

    i_should_cook: A smart cook book in React Native and Tensorflow

---
+++ i_should_cook/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Camera from 'react-native-camera';
+
+export default class App extends React.Component {
+
+   takePicture= () =>{
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+
+    
+
+    return (
+      <View style={styles.container}>
+        <Text>Welcome</Text>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  }
+});
